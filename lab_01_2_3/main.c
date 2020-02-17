af0855fa4b60df207e576b8eb9e727137275b157
Klimenko Alexey <klimenko0037@gmail.com>
2020-02-17 22:01:07 +0300

    Upload main.c

---
+++ lab_01_2_3/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float R1, R2, R3;
+    float R;
+
+    scanf("%f %f %f", &R1, &R2, &R3);
+    R = R1 * R2 * R3 / (R1 * R2 + R1 * R3 + R2 * R3);
+    printf("Total resistance = %.3f", R);
+    return 0;
+}
