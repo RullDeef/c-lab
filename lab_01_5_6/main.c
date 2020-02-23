6a8b3c8a57c510aa429de2dbe3169a02918e5ff4
Klimenko Alexey <klimenko0037@gmail.com>
2020-02-23 16:26:08 +0300

    Fixed build errors in lab_01_5_6

--- lab_01_5_6/main.c
+++ lab_01_5_6/main.c
@@ -3,57 +3,58 @@
 #include <math.h>
 
 
-float area(float x1, float y1, float x2, float y2, float x3, float y3)
+typedef struct {
+    float x, y;
+} point_t;
+
+
+point_t subtract(point_t a, point_t b)
 {
-    return (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
-}
-
-bool sub_intersect(float a, float b, float c, float d)
-{
-    if (a > b)
-    {
-        b += a;
-        a = b - a;
-        b -= a;
-    }
-
-    if (c > d)
-    {
-        d += c;
-        c = d - c;
-        d -= c;
-    }
-
-    return fmaxf(a, c) <= fminf(b, d);
-}
-
-bool intersect(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
-{
-    return sub_intersect(x1, x2, x3, x4) && sub_intersect(y1, y2, y3, y4)
-        && area(x1, y1, x2, y2, x3, y3) * area(x1, y1, x2, y2, x4, y4) <= 0
-        && area(x3, y3, x4, y4, x1, y1) * area(x3, y3, x4, y4, x2, y2) <= 0;
+    point_t res;
+    res.x = a.x - b.x;
+    res.y = a.y - b.y;
+    return res;
 }
 
 
-bool input_point(float *x, float *y)
+float slant_product(point_t a, point_t b)
+{
+    return a.x * b.y - b.x * a.y;
+}
+
+
+/**
+ * Checks if points c and d lays on different sides of line ab
+ */
+bool intersect_line(point_t a, point_t b, point_t c, point_t d)
+{
+    point_t v1 = subtract(b, a);
+    point_t v2 = subtract(c, a);
+    point_t v3 = subtract(d, a);
+
+    float s1 = slant_product(v2, v1);
+    float s2 = slant_product(v1, v3);
+
+    return s1 * s2 > 0;
+}
+
+
+bool input_point(point_t *p)
 {
     char c;
-    int argc = scanf("%f%c%f%c", x, &c, y, &c);
-
+    int argc = scanf("%f%c%f%c", &p->x, &c, &p->y, &c);
     return (argc != 4 && argc != EOF) || c != '\n';
 }
 
 
 int main()
 {
-    float x1, y1;
-    float x2, y2;
-    float x3, y3;
-    float x4, y4;
+    point_t a, b, c, d;
+    bool res;
 
     printf("Input point A(x, y): ");
 
-    if (input_point(&x1, &y1))
+    if (input_point(&a))
     {
         printf("Incorrect input. Closing program...\n");
         return 1;
@@ -61,7 +62,7 @@ int main()
 
     printf("Input point B(x, y): ");
 
-    if (input_point(&x2, &y2))
+    if (input_point(&b))
     {
         printf("Incorrect input. Closing program...\n");
         return 1;
@@ -69,7 +70,7 @@ int main()
 
     printf("Input point C(x, y): ");
 
-    if (input_point(&x3, &y3))
+    if (input_point(&c))
     {
         printf("Incorrect input. Closing program...\n");
         return 1;
@@ -77,13 +78,15 @@ int main()
 
     printf("Input point D(x, y): ");
 
-    if (input_point(&x4, &y4))
+    if (input_point(&d))
     {
         printf("Incorrect input. Closing program...\n");
         return 1;
     }
 
-    printf("Intersect: %d", intersect(x1, y1, x2, y2, x3, y3, x4, y4));
+    res = intersect_line(a, b, c, d) && intersect_line(c, d, a, b);
+
+    printf("Intersect: %d", res);
 
     return 0;
 }
