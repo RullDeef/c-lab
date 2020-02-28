5fad3defe95987d7fdbf625cbd72c8c470eb1541
Klimenko Alexey <klimenko0037@gmail.com>
2020-02-28 17:55:48 +0300

    changed struct initializers.

--- lab_01_5_6/main.c
+++ lab_01_5_6/main.c
@@ -6,6 +6,15 @@
 #include "mat.h"
 
 
+/**
+ * returns a <= x <= b with epsilon from mat.h
+ */
+bool in_range(float x, float a, float b)
+{
+    return (a - _MAT_EPS < x) && (x < b + _MAT_EPS);
+}
+
+
 bool input_points(vec2_t arr[], const int size)
 {
     char c;
@@ -26,7 +35,6 @@ int main()
 {
     vec2_t a, b, c, d;
     vec2_t arr[4];
-    bool res = false;
 
     if (input_points(arr, 4))
     {
@@ -34,32 +42,42 @@ int main()
         return 1;
     }
 
-    a = arr[0]; b = arr[1]; c = arr[2]; d = arr[3];
+    a = arr[0];
+    b = arr[1];
+    c = arr[2];
+    d = arr[3];
 
     vec_printf(a);
     vec_printf(b);
     vec_printf(c);
     vec_printf(d);
 
-    mat2x2_t mat = mat_from_vec2(vec_sub(b, a), vec_sub(d, c));
-
+    vec2_t u = vec_sub(b, a);
+    vec2_t v = vec_sub(d, c);
+    mat2x2_t mat = mat_from_vec2(u, v);
     mat_printf(mat);
 
     float det = mat_det(mat);
 
-    if (fabsf(det) > _MAT_EPS)
+    if (vec_length(u) < _MAT_EPS || vec_length(v) < _MAT_EPS)
+    {
+        // printf("Some pair of points are the same.");
+        return 1;
+    }
+    else if (fabsf(det) < _MAT_EPS)
+    {
+        // printf("Lines are parallel.");
+        return 1;
+    }
+    else
     {
         vec2_t st = mat_apply(mat_inverse(mat), vec_sub(c, a));
-
         vec_printf(st);
 
-        res = ((-_MAT_EPS < st.x && st.x < 1 + _MAT_EPS) &&
-            (-1 - _MAT_EPS < st.y && st.y < _MAT_EPS)) ||
-                ((-_MAT_EPS < st.y && st.y < 1 + _MAT_EPS) &&
-                    (-1 - _MAT_EPS < st.x && st.x < _MAT_EPS));
+        bool res = (in_range(st.x, 0, 1) && in_range(st.y, -1, 0)) ||
+            (in_range(st.x, -1, 0) && in_range(st.y, 0, 1));
+        
+        printf("%d", res);
+        return 0;
     }
-
-    printf("%d", res);
-
-    return 0;
 }
--- lab_01_5_6/mat.h
+++ lab_01_5_6/mat.h
@@ -5,46 +5,54 @@
 #define _MAT_EPS 0.0000001f
 #endif
 
+#include <math.h>
+
 #ifdef _MAT_PRINTF
 #include <stdio.h>
 #endif
 
-typedef float __num_t;
+typedef float mat_num_t;
 
 
 typedef struct
 {
-    __num_t x, y;
+    mat_num_t x, y;
 } vec2_t;
 
 
-#define zero_vec2  (vec2_t) { 0, 0 };
-#define one_vec2   (vec2_t) { 1, 1 };
-
-
 typedef struct
 {
-    __num_t data[4];
+    mat_num_t data[4];
 } mat2x2_t;
 
 
-#define zero_mat     (mat2x2_t) {{ 0, 0, 0, 0 }};
-#define identity_mat (mat2x2_t) {{ 1, 0, 0, 1 }};
+mat_num_t vec_length(vec2_t vec)
+{
+    mat_num_t x2 = vec.x * vec.x;
+    mat_num_t y2 = vec.y * vec.y;
+    return sqrtf(x2 + y2);
+}
 
 
 vec2_t vec_add(vec2_t vec1, vec2_t vec2)
 {
-    vec2_t res = (vec2_t) { vec1.x + vec2.x, vec1.y + vec2.y };
+    vec2_t res;
+    res.x = vec1.x + vec2.x;
+    res.y = vec1.y + vec2.y;
     return res;
 }
+
 
 vec2_t vec_sub(vec2_t vec1, vec2_t vec2)
 {
-    vec2_t res = (vec2_t) { vec1.x - vec2.x, vec1.y - vec2.y };
+    vec2_t res;
+    res.x = vec1.x - vec2.x;
+    res.y = vec1.y - vec2.y;
     return res;
 }
 
-__num_t mat_det(mat2x2_t mat)
+
+mat_num_t mat_det(mat2x2_t mat)
 {
     return mat.data[0] * mat.data[3] - mat.data[1] * mat.data[2];
 }
@@ -52,40 +60,55 @@ __num_t mat_det(mat2x2_t mat)
 
 mat2x2_t mat_mult(mat2x2_t mat1, mat2x2_t mat2)
 {
-    mat2x2_t res = (mat2x2_t) {{
-        mat1.data[0] * mat2.data[0] + mat1.data[1] * mat2.data[2],
-        mat1.data[0] * mat2.data[1] + mat1.data[1] * mat2.data[3],
-        mat1.data[2] * mat2.data[0] + mat1.data[3] * mat2.data[2],
-        mat1.data[2] * mat2.data[1] + mat1.data[3] * mat2.data[3] }};
+    mat2x2_t res;
+    res.data[0] = mat1.data[0] * mat2.data[0] + mat1.data[1] * mat2.data[2];
+    res.data[1] = mat1.data[0] * mat2.data[1] + mat1.data[1] * mat2.data[3];
+    res.data[2] = mat1.data[2] * mat2.data[0] + mat1.data[3] * mat2.data[2];
+    res.data[3] = mat1.data[2] * mat2.data[1] + mat1.data[3] * mat2.data[3];
     return res;
 }
 
 
 vec2_t mat_apply(mat2x2_t mat, vec2_t vec)
 {
-    vec2_t res = (vec2_t) {
-        mat.data[0] * vec.x + mat.data[1] * vec.y,
-        mat.data[2] * vec.x + mat.data[3] * vec.y };
+    vec2_t res;
+    res.x = mat.data[0] * vec.x + mat.data[1] * vec.y;
+    res.y = mat.data[2] * vec.x + mat.data[3] * vec.y;
     return res;
 }
 
 
 mat2x2_t mat_inverse(mat2x2_t mat)
 {
-    __num_t det = mat_det(mat);
+    mat_num_t det = mat_det(mat);
+    mat2x2_t res;
 
     if (-_MAT_EPS < det && det < _MAT_EPS)
-        return zero_mat;
+    {
+        res.data[0] = 0;
+        res.data[1] = 0;
+        res.data[2] = 0;
+        res.data[3] = 0;
+    }
+    else
+    {
+        res.data[0] =  mat.data[3] / det;
+        res.data[1] = -mat.data[1] / det;
+        res.data[2] = -mat.data[2] / det;
+        res.data[3] =  mat.data[0] / det;
+    }
     
-    mat2x2_t res = (mat2x2_t) {{ mat.data[3] / det, -mat.data[1] / det,
-        -mat.data[2] / det, mat.data[0] / det }};
     return res;
 }
 
 
 mat2x2_t mat_from_vec2(vec2_t vec1, vec2_t vec2)
 {
-    mat2x2_t res = (mat2x2_t) {{ vec1.x, vec2.x, vec1.y, vec2.y }};
+    mat2x2_t res;
+    res.data[0] = vec1.x;
+    res.data[1] = vec2.x;
+    res.data[2] = vec1.y;
+    res.data[3] = vec2.y;
     return res;
 }
 
