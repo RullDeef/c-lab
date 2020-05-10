2a3ab152031971848aa0e1ba845bfc144a6df263
Klimenko Alexey <klimenko0037@gmail.com>
2020-05-10 20:38:00 +0300

    quick fix

--- lab_51_4_1/main.c
+++ lab_51_4_1/main.c
@@ -9,8 +9,7 @@
 typedef enum
 {
     exit_success = 0,
-    expected_end_of_input,
-    unexpected_end_of_input,
+    end_of_input,
     invalid_input,
     too_few_numbers
 } status_code_t;
@@ -26,23 +25,10 @@ status_code_t get_next_number(int numbers[NUM_CHUNK_LENGTH])
     // try to read and push back new number
     int readed = fscanf(stdin, "%d", &numbers[NUM_CHUNK_LENGTH - 1]);
     
-    if (readed == EOF)
-        status_code = unexpected_end_of_input;
-    
-    else if (readed == 1) // successfully readed 1 number from stdin
+    if (readed == 1) // successfully readed 1 number from stdin
         status_code = exit_success;
-    
     else
-    {
-        char str[2] = "";
-        readed = fscanf(stdin, " %2s", str);
-
-        if (isalpha(str[0]) && str[1] == '\0')
-            status_code = expected_end_of_input;
-        
-        else
-            status_code = invalid_input;
-    }
+        status_code = end_of_input;
 
     return status_code;
 }
@@ -54,7 +40,7 @@ bool mid_is_maximum(const int numbers[NUM_CHUNK_LENGTH])
 
 int main(void)
 {
-    int numbers[NUM_CHUNK_LENGTH] = {0, 0, 0};
+    int numbers[NUM_CHUNK_LENGTH] = { 0, 0, 0 };
     int8_t readed_chunk_length = 0;
     size_t maximums_amount = 0;
 
@@ -79,8 +65,7 @@ int main(void)
 
     if (readed_chunk_length < NUM_CHUNK_LENGTH)
         status_code = too_few_numbers;
-
-    else if (status_code == expected_end_of_input)
+    else if (status_code == end_of_input)
     {
         fprintf(stdout, "%zu", maximums_amount);
         status_code = exit_success;
