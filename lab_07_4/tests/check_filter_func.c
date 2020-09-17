b21aa4ffef7980a039a421ef5f9375e50bd292e1
Klimenko Alexey <klimenko0037@gmail.com>
2020-09-17 09:41:03 +0300

    add unittests

---
+++ lab_07_4/tests/check_filter_func.c
@@ -0,0 +1,55 @@
+#include <check.h>
+#include <stdlib.h>
+#include "./inc/filter_func.h"
+
+
+START_TEST(begin_eq_end)
+{
+    int begin[] = { 0, 1, 2, 3, 4 };
+
+    int *filtered_begin;
+    int *filtered_end;
+
+    ck_assert_int_ne(key(begin, begin, &filtered_begin, &filtered_end), 0);
+}
+END_TEST
+
+START_TEST(empty_out)
+{
+    int begin[] = { 5, 5, 5, 5, 5 };
+    int *end = begin + sizeof(begin) / sizeof(int);
+
+    int *filtered_begin;
+    int *filtered_end;
+
+    ck_assert_int_ne(key(begin, end, &filtered_begin, &filtered_end), 0);
+}
+END_TEST
+
+START_TEST(base_case)
+{
+    int begin[] = { 3, 5, 7, 2, 1, 8, 3 };
+    int *end = begin + sizeof(begin) / sizeof(int);
+
+    int expected[] = { 5, 7, 8 };
+    int expected_size = sizeof(expected) / sizeof(int);
+
+    int *filtered_begin;
+    int *filtered_end;
+
+    ck_assert_int_eq(key(begin, end, &filtered_begin, &filtered_end), 0);
+
+    // check output array size
+    ck_assert_int_eq(filtered_end - filtered_begin, expected_size);
+
+    for (int i = 0; i < expected_size; i++)
+        ck_assert_int_eq(filtered_begin[i], expected[i]);
+}
+END_TEST
+
+void check_filter_func(TCase *tc_core)
+{
+    tcase_add_test(tc_core, begin_eq_end);
+    tcase_add_test(tc_core, empty_out);
+    tcase_add_test(tc_core, base_case);
+}
---
+++ lab_07_4/tests/check_sort_func.c
@@ -0,0 +1,39 @@
+#include <check.h>
+#include "./inc/sort_func.h"
+
+START_TEST(single_element)
+{
+    int array[] = { 24 };
+
+    ck_assert_int_eq(mysort(array, 1, sizeof(int), int_comparator), 0);
+    ck_assert_int_eq(array[0], 24);
+}
+END_TEST
+
+START_TEST(two_elements)
+{
+    int array[] = { 12, 4 };
+
+    ck_assert_int_eq(mysort(array, 2, sizeof(int), int_comparator), 0);
+    ck_assert_int_eq(array[0], 4);
+    ck_assert_int_eq(array[1], 12);
+}
+END_TEST
+
+START_TEST(repeating_elements)
+{
+    int array[] = { 15, 15, 15 };
+
+    ck_assert_int_eq(mysort(array, 3, sizeof(int), int_comparator), 0);
+    ck_assert_int_eq(array[0], 15);
+    ck_assert_int_eq(array[1], 15);
+    ck_assert_int_eq(array[2], 15);
+}
+END_TEST
+
+void check_sort_func(TCase *tc_core)
+{
+    tcase_add_test(tc_core, single_element);
+    tcase_add_test(tc_core, two_elements);
+    tcase_add_test(tc_core, repeating_elements);
+}
