2785ce2140b2ad7af1ee78bf05b15c81c6a66d7c
Klimenko Alexey <klimenko0037@gmail.com>
2020-10-08 00:43:12 +0100

    setup auto test

---
+++ lab_08_9_12/func_tests/test.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# positive tests counters
+declare -i total_pos=$(ls -1q ./func_tests/pos_*_args.txt | wc -l)
+declare -i passed_pos=0
+
+# test positive cases
+printf "  Позитивные тесты:\n\n"
+for ((i = 1; i <= $total_pos; i++))
+do
+  # gather expected and actual strings
+  n=$(printf "%02d" $i)
+  expected=$(cat "./func_tests/pos_${n}_out.txt")
+  args=$(cat "./func_tests/pos_${n}_args.txt")
+  ./app.exe $args .temp_$n
+  status=$?
+  actual=$(cat .temp_$n)
+
+  if [ $status -ne 0 ]
+  then
+    echo "Тест #$i: Упал с кодом $status" 
+  else
+    diff -q "./func_tests/pos_${n}_out.txt" ".temp_$n"
+    if [ $? -eq 0 ]
+    then
+      passed_pos=$(( passed_pos + 1 ))
+      echo "Тест #$i: Прошёл."
+    else
+      echo "Тест #$i: Упал."
+      echo "Ожидалось: $expected"
+      echo "Вместо: $actual"
+    fi
+  fi
+done
+
+# rm -rf ./.temp_*
+
+# negative tests counters
+declare -i total_neg=$(ls -1q ./func_tests/neg_*_args.txt 2>/dev/null | wc -l)
+declare -i passed_neg=0
+
+# test negative cases
+printf "\n  Негативные тесты:\n\n"
+for ((i = 1; i <= $total_neg; i++))
+do
+  # gather expected and actual strings
+  n=$(printf "%02d" $i)
+  expected=$(cat "./func_tests/neg_${n}_out.txt")
+  args=$(cat "./func_tests/neg_${n}_args.txt")
+  ./app.exe $args 2>.temp_$n 1>.temp_$n
+  status=$?
+  actual=$(cat .temp_$n)
+
+  if [ $status = 0 ]
+  then
+    echo "Тест #$i: Упал с кодом $status" 
+  else
+    passed_neg=$(( passed_neg + 1 ))
+    echo "Test #$i: Прошёл."
+  fi
+done
+
+rm -rf ./.temp_*
+
+echo
+echo "Позитивных тестов: $total_pos всего, $passed_pos прошло, $(( total_pos - passed_pos )) упало."
+echo "Негативных тестов: $total_neg всего, $passed_neg прошло, $(( total_neg - passed_neg )) упало."
+
+printf "\n  Сбор инормации о покрытии...\n"
+
+# move to proper place
+for srcf in $(find ./src/ -name *.c)
+do
+    dir=$(dirname $srcf)
+    srcf_base=$(basename $srcf)
+    f="${srcf_base%.*}"
+
+    mv ./obj/$f.gcda $dir/$f.gcda
+    mv ./obj/$f.gcno $dir/$f.gcno
+done
+
+declare -i percentage=0
+declare -i amount=$(find ./src -name *.c | wc -l)
+for f in $(find ./src -name *.c); do
+  mkdir -p ./func_tests/gcov/$(dirname $f)
+  base_name=$(basename $f)
+  out_file="./func_tests/gcov/src/.$(dirname $f)/$base_name.gcov"
+  gcov "$f" -n -t > $out_file
+  #percentage_i=`cat $out_file | grep -E -o "[0-9]+" | head -1`
+  percentage_i=`gcov "$f" -n | grep -E -o "[0-9]+" | head -1`
+  percentage=$(( percentage + percentage_i / amount ))
+done
+echo "Покрытие кода тестами: $percentage%"
+
+# clean up everything except *.c.gcov
+find ./src -name *.gcno -delete
+find ./src -name *.gcda -delete
